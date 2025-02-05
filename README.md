c2987f8589e7dbbb40a48231aa9520d53476af5d
Simon Willison <swillison@gmail.com>
2025-02-04 20:47:12 -0800

    Use internal DB, added support for publish database
    Refs #6, #9

--- README.md
+++ README.md
@@ -10,47 +10,34 @@ Make specific Datasette tables visible to the public
 ## Installation
 
 Install this plugin in the same environment as Datasette.
-
-    datasette install datasette-public
-
+```bash
+datasette install datasette-public
+```
 ## Usage
 
-Any tables listed in the `_public_tables` table will be visible to the public, even if the rest of the Datasette instance does not allow anonymous access.
+This plugin can only be used with Datasette 1.0a+ and requires Datasette to be run with a persistent internal database:
 
-The root user (and any user with the new `public-tables` permission) will get a new option in the table action menu allowing them to toggle a table between public and private.
-
-Installing this plugin also causes `allow-sql` permission checks to fall back to checking if the user has access to the entire database. This is to avoid users with access to a single public table being able to access data from other tables using the `?_where=` query string parameter.
-
-## Configuration
-
-This plugin creates a new table in one of your databases called `_public_tables`.
-
-This table defaults to being created in the first database passed to Datasette.
-
-To create it in a different named database, use this plugin configuration:
-
-```json
-{
-  "plugins": {
-    "datasette-public": {
-      "database": "database_to_create_table_in"
-    }
-  }
-}
+```bash
+datasette --internal internal.db data.db
 ```
+
+A UI allows users with the `datasette-public` permission to toggle both tables and databases between public and private.
+
+TODO: Make this true: Installing this plugin also causes `allow-sql` permission checks to fall back to checking if the user has access to the entire database. This is to avoid users with access to a single public table being able to access data from other tables using the `?_where=` query string parameter.
 
 ## Development
 
 To set up this plugin locally, first checkout the code. Then create a new virtual environment:
-
-    cd datasette-public
-    python3 -m venv venv
-    source venv/bin/activate
-
+```bash
+cd datasette-public
+python -m venv venv
+source venv/bin/activate
+```
 Now install the dependencies and test dependencies:
-
-    pip install -e '.[test]'
-
+```bash
+pip install -e '.[test]'
+```
 To run the tests:
-
-    pytest
+```bash
+pytest
+```
--- datasette_public/__init__.py
+++ datasette_public/__init__.py
@@ -1,18 +1,27 @@
 from datasette import hookimpl, Forbidden, Response, NotFound
 from urllib.parse import quote_plus, unquote_plus
+from typing import Optional, Tuple
 
-CREATE_TABLE_SQL = """
-create table _public_tables (table_name text primary key)
+CREATE_TABLES_SQL = """
+create table if not exists public_tables (
+    database_name text,
+    table_name text,
+    primary key (database_name, table_name)
+);
+create table if not exists public_databases (
+    database_name text primary key,
+    allow_sql integer default 0
+);
 """.strip()
 
 
 @hookimpl
 def startup(datasette):
     async def inner():
-        db = db_from_config(datasette)
-        assert db.is_mutable, "Database is immutable"
-        if "_public_tables" not in await db.table_names():
-            await db.execute_write(CREATE_TABLE_SQL)
+        db = datasette.get_internal_database()
+        if db.is_memory:
+            raise ValueError("datasette-public requires a persistent database")
+        await db.execute_write_script(CREATE_TABLES_SQL)
 
     return inner
 
@@ -21,66 +30,123 @@ def startup(datasette):
 def permission_allowed(datasette, action, actor, resource):
     async def inner():
         # Root actor can always edit public status
-        if actor and actor.get("id") == "root" and action == "public-tables":
+        if actor and actor.get("id") == "root" and action == "datasette-public":
             return True
         if action == "execute-sql" and not actor:
-            return await datasette.permission_allowed(
-                actor, "view-database", resource=resource
+            is_public, allow_sql = await database_privacy_settings(datasette, resource)
+            print(
+                "Check for actor",
+                actor,
+                "is_public",
+                "resource",
+                resource,
+                " - result: is_public",
+                is_public,
+                "allow_sql",
+                allow_sql,
             )
-        if action != "view-table":
+            if not allow_sql:
+                return allow_sql
+        if action not in ("view-table", "view-database"):
             return None
-        # Say 'yes' if this table is public
-        database_name, table_name = resource
-        db = db_from_config(datasette)
-        if await table_is_public(db, table_name):
+        if action == "view-table" and await table_is_public(
+            datasette, resource[0], resource[1]
+        ):
             return True
+        if action == "view-database":
+            is_public, _ = await database_privacy_settings(datasette, resource)
+            if is_public:
+                return True
 
     return inner
 
 
-async def table_is_public(db, table_name):
-    # TODO: include database_name in check
+async def table_is_public(datasette, database_name, table_name):
+    db = datasette.get_internal_database()
     rows = await db.execute(
-        "select 1 from _public_tables where table_name = ?", [table_name]
+        "select 1 from public_tables where database_name = ? and table_name = ?",
+        (database_name, table_name),
     )
-    if len(rows):
-        return True
+    return bool(len(rows))
+
+
+async def database_privacy_settings(datasette, database_name) -> Tuple[bool, bool]:
+    db = datasette.get_internal_database()
+    result = await db.execute(
+        "select 1, allow_sql from public_databases where database_name = ?",
+        [database_name],
+    )
+    row = result.first()
+    if not row:
+        return (False, False)
+    return (True, bool(row["allow_sql"]))
 
 
 @hookimpl
 def table_actions(datasette, actor, database, table):
     async def inner():
         if not await datasette.permission_allowed(
-            actor, "public-tables", resource=database, default=False
+            actor, "datasette-public", resource=database, default=False
         ):
             return []
-        if database != "_internal":
-            noun = "table"
-            if table in await datasette.get_database(database).view_names():
-                noun = "view"
-            is_private = not await table_is_public(db_from_config(datasette), table)
-            return [
-                {
-                    "href": datasette.urls.path(
-                        "/-/public-table/{}/{}".format(database, quote_plus(table))
-                    ),
-                    "label": "Make {} {}".format(
-                        noun, "public" if is_private else "private"
-                    ),
-                    "description": (
-                        "Allow anyone to view this {}".format(noun)
-                        if is_private
-                        else "Only allow logged-in users to view this {}".format(noun)
-                    ),
-                }
-            ]
+        noun = "table"
+        if table in await datasette.get_database(database).view_names():
+            noun = "view"
+        is_private = not await table_is_public(datasette, database, table)
+        return [
+            {
+                "href": datasette.urls.path(
+                    "/-/public-table/{}/{}".format(database, quote_plus(table))
+                ),
+                "label": "Make {} {}".format(
+                    noun, "public" if is_private else "private"
+                ),
+                "description": (
+                    "Allow anyone to view this {}".format(noun)
+                    if is_private
+                    else "Only allow logged-in users to view this {}".format(noun)
+                ),
+            }
+        ]
 
     return inner
 
 
+@hookimpl
+def database_actions(datasette, actor, database):
+    async def inner():
+        if not await datasette.permission_allowed(
+            actor, "datasette-public", resource=database, default=False
+        ):
+            return []
+        is_public, _ = await database_privacy_settings(datasette, database)
+        return [
+            {
+                "href": datasette.urls.path(
+                    "/-/public-database/{}".format(quote_plus(database))
+                ),
+                "label": "Make database {}".format(
+                    "private" if not is_public else "public"
+                ),
+                "description": (
+                    "Only allow logged-in users to view this database"
+                    if is_public
+                    else "Allow anyone to view this database"
+                ),
+            }
+        ]
+
+    return inner
+
+
+@hookimpl
+def view_actions(datasette, actor, database, view):
+    return table_actions(datasette, actor, database, view)
+
+
 async def check_permissions(datasette, request, database):
-    if database == "_internal" or not await datasette.permission_allowed(
-        request.actor, "public-tables", resource=database, default=False
+    if not await datasette.permission_allowed(
+        request.actor, "datasette-public", resource=database, default=False
     ):
         raise Forbidden("Permission denied for changing table privacy")
 
@@ -99,7 +165,7 @@ async def change_table_privacy(request, datasette):
     ):
         raise NotFound("{} not found".format(noun))
 
-    permission_db = db_from_config(datasette)
+    permission_db = datasette.get_internal_database()
 
     if request.method == "POST":
         form_data = await request.post_vars()
@@ -107,17 +173,19 @@ async def change_table_privacy(request, datasette):
         if action == "make-public":
             msg = "public"
             await permission_db.execute_write(
-                "insert or ignore into _public_tables (table_name) values (?)", [table]
+                "insert or ignore into public_tables (database_name, table_name) values (?, ?)",
+                [database_name, table],
             )
         elif action == "make-private":
             msg = "private"
             await permission_db.execute_write(
-                "delete from _public_tables where table_name = ?", [table]
+                "delete from public_tables where database_name = ? and table_name = ?",
+                [database_name, table],
             )
         datasette.add_message(request, "{} '{}' is now {}".format(noun, table, msg))
         return Response.redirect(datasette.urls.table(database_name, table))
 
-    is_private = not await table_is_public(permission_db, table)
+    is_private = not await table_is_public(datasette, database_name, table)
 
     return Response.html(
         await datasette.render_template(
@@ -133,6 +201,46 @@ async def change_table_privacy(request, datasette):
     )
 
 
+async def change_database_privacy(request, datasette):
+    database_name = request.url_vars["database"]
+    await check_permissions(datasette, request, database_name)
+    permission_db = datasette.get_internal_database()
+
+    if request.method == "POST":
+        form_data = await request.post_vars()
+        allow_sql = bool(form_data.get("allow_sql"))
+        action = form_data.get("action")
+        if action == "make-public":
+            msg = "public"
+            await permission_db.execute_write(
+                "insert or replace into public_databases (database_name, allow_sql) values (?, ?)",
+                (database_name, allow_sql),
+            )
+        elif action == "make-private":
+            msg = "private"
+            await permission_db.execute_write(
+                "delete from public_databases where database_name = ?", [database_name]
+            )
+        datasette.add_message(
+            request, "Database '{}' is now {}".format(database_name, msg)
+        )
+        return Response.redirect(datasette.urls.database(database_name))
+
+    is_public, allow_sql = await database_privacy_settings(datasette, database_name)
+
+    return Response.html(
+        await datasette.render_template(
+            "public_database_change_privacy.html",
+            {
+                "database": database_name,
+                "is_private": not is_public,
+                "allow_sql": allow_sql,
+            },
+            request=request,
+        )
+    )
+
+
 @hookimpl
 def register_routes():
     return [
@@ -140,10 +248,8 @@ def register_routes():
             r"^/-/public-table/(?P<database>[^/]+)/(?P<table>[^/]+)$",
             change_table_privacy,
         ),
+        (
+            r"^/-/public-database/(?P<database>[^/]+)$",
+            change_database_privacy,
+        ),
     ]
-
-
-def db_from_config(datasette):
-    config = datasette.plugin_config("datasette-public") or {}
-    db_name = config.get("database") or None
-    return datasette.get_database(db_name)
